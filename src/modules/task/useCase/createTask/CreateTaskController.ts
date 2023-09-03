7ced68fa802db4ed7e503c031bac6e63e91ac9c4
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-09-02 21:55:10 -0300

    feat: Implement task creation data validation

--- src/modules/task/useCase/createTask/CreateTaskController.ts
+++ src/modules/task/useCase/createTask/CreateTaskController.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import { z } from 'zod';
 
 import { CreateTaskUseCase, IRequest } from './CreateTaskUseCase';
+
 import { CreateTaskSchema } from './CreateTaskValidation';
 
 class CreateTaskController {
@@ -10,14 +12,23 @@ class CreateTaskController {
             const { name, description }: IRequest = request.body;
             const user_id = request.user.id;
 
-            CreateTaskSchema.parse(request.body);
+            const transformedData = CreateTaskSchema.parse({ name, description });
 
             const createTaskUserCase = container.resolve(CreateTaskUseCase);
 
-            const task = await createTaskUserCase.execute({ name, description, user_id });
+            const task = await createTaskUserCase.execute({
+                name: transformedData.name,
+                description: transformedData.description,
+                user_id,
+            });
 
             return response.status(201).json(task);
         } catch (err) {
+            if (z.instanceof(err)) {
+                const errorMessages = err.issues.map((issue) => issue.message);
+                return response.status(400).json({ errors: errorMessages });
+            }
+
             return response.status(500).json({ error: 'It was not possible to create a new task' + err.message });
         }
     }
