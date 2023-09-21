2d802ddbbbb102c6efa6369a94b429d6a93c322e
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-09-20 21:39:23 -0300

    Update IUsersRepository.ts

--- src/modules/account/repositories/IUsersRepository.ts
+++ src/modules/account/repositories/IUsersRepository.ts
@@ -2,7 +2,7 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { User } from '../infra/typeorm/entities/User';
 
 interface IUsersRepository {
-    create({ id, name, email, password }: ICreateUserDTO): Promise<User>;
+    create({ id, name, email, password }: ICreateUserDTO): Promise<void>;
     update(id: string, name: string, email: string, password: string): Promise<User | undefined>;
     delete(id: string): Promise<void>;
     findById(id: string): Promise<User | undefined>;
